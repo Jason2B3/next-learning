995d1aa9c43156e8815b7654dba9180970a70b2f
jason <2B3>
2021-09-30 17:09:54 -0400

    facebook mock example

---
+++ pages/[id]/fileA.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function fileA() {
+  return <h1>File A in [id] folder</h1>;
+}
+
+export default fileA;
---
+++ pages/[id]/fileB.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function fileB() {
+  return <h1>File B in [id] folder</h1>;
+}
+
+export default fileB;
