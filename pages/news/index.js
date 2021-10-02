515c9907b2173b20dd1efbcd94c8529de415dad9
jason <2B3>
2021-10-02 05:03:38 -0400

    commit n85

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,17 +1,39 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-
+import React, { useEffect } from "react";
 export default function News() {
-  const reports = [
-    { id: 1, story: "bank robbery" },
-    { id: 2, story: "Lotto win" },
-  ];
-  const router = useRouter();
-  const btnHandler = () => router.replace("/blog");
+  const getData = async () => {
+    // your fetch URL should be relative to the root level of the public folder
+    const res = await fetch("jsonFiles/data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    console.log(res);
+    const parsed = await res.json();
+    console.log(parsed)
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <h1>The "news" path HomePage</h1>
-      <button onClick={btnHandler}>Press me to go to the /blog page</button>
     </div>
   ); // produces links to /news/1 and /news/2
 }
+/*
+
+  const fetch= async function(){
+    const response= await fetch("/backend.json")
+    if(!response.ok) alert(4044)
+    let parsedResponse= await response.json()
+    return parsedResponse
+  }
+  useEffect(()=>{
+    
+    // const fetchResults= console.log(fetch())
+    fetch()
+  },[])
+*/
