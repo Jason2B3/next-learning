123232d8cf48e0cb9d0069124d86bca4f1ca54fb
jason <2B3>
2021-10-02 00:34:46 -0400

    commit n63

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,4 @@
-import React from "react";
 
 export default function index() {
   return <div>Blog Home Page</div>;
-}
-import React from "react";
-
-export default function anyID() {
-  return <div>dynamic</div>;
 }
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,21 +1,17 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function News() {
   const reports = [
     { id: 1, story: "bank robbery" },
     { id: 2, story: "Lotto win" },
   ];
+  const router = useRouter();
+  const btnHandler = () => router.replace("/blog");
   return (
     <div>
       <h1>The "news" path HomePage</h1>
-      <Link
-        href={{
-          pathname: "/news/[newsId]",
-          query: { newsId: 1 },
-        }}
-      >
-        1
-      </Link>
+      <button onClick={btnHandler}>Press me to go to the /blog page</button>
     </div>
   ); // produces links to /news/1 and /news/2
 }
